46fab6a68eb8e3af10b5a9a7987e01792eb45d79
supa <singh41958@gmail.com>
2025-09-28 18:39:26 -0400

    Update prerelease.lua

--- prerelease.lua
+++ prerelease.lua
@@ -1,27 +1,30 @@
--- Load Venyx UI
 local Venyx = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/Venyx-UI-Library/main/source.lua"))()
 
--- Create the main window
-local UI = Venyx.new("Test Window", 5012544693) -- second arg is optional icon ID
+local UI = Venyx.new("JCJM Hub | Steal a Brainrot", 5012544693)
 
--- Pages (Tabs)
-local pageStealing = UI:addPage("Stealing", 5012544693)
+local pageBrainrots = UI:addPage("Brainrots", 5012544693)
 local pageVisuals  = UI:addPage("Visuals", 5012544693)
 local pageMisc     = UI:addPage("Misc", 5012544693)
 local pageSettings = UI:addPage("Settings", 5012544693)
 
--- Sections and placeholders
-local stealingSection = pageStealing:addSection("Placeholder")
-stealingSection:addToggle("Placeholder", false, function(v) end)
+local stealingSection = pageBrainrots:addSection("Stealing")
+stealingSection:addButton("Tween to Base Steal (method 1)", function() end)
+stealingSection:addButton("Tween to Base Steal (method 2)", function() end)  
+stealingSection:addButton("Float to Base", function() end)
+stealingSection:addButton("Laser Steal (requires rebirth 9)", function() end)
+stealingSection:addButton("Move Up 5 Studs", function() end)
+stealingSection:addButton("Move Down 5 Studs", function() end)
+stealingSection:addToggle("Steal Boost", false, function(v) end)
+local buyingSection = pageBrainrots:addSection("Buying")
+local brainrotsSection = pageBrainrots:addSection("Finder")
 
-local visualsSection = pageVisuals:addSection("Placeholder")
+local visualsSection = pageVisuals:addSection("Visuals")
 visualsSection:addSlider("Placeholder", 1, 10, 5, function(v) end)
 
-local tpSection = pageTP:addSection("Placeholder")
-tpSection:addButton("Placeholder", function() end)
+local miscSection = pageMisc:addSection("")
+miscSection:addButton("Placeholder", function() end)
 
-local infoSection = pageInfo:addSection("Placeholder")
-infoSection:addTextbox("Placeholder", "Default", function(v) end)
+local settingsSection = pageSettings:addSection("Settings")
+settingsSection:addTextbox("Placeholder", "Default", function(v) end)
 
--- Show the UI
 UI:SelectPage(UI.pages[1], true)
