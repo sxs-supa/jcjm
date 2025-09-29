c1a5c00b5bec12faca104fefb1bceca20d0bbe6d
supa <singh41958@gmail.com>
2025-09-28 20:46:09 -0400

    Update prerelease.lua

--- prerelease.lua
+++ prerelease.lua
@@ -1,102 +1,101 @@
-local Venyx = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/Venyx-UI-Library/main/source.lua"))()
 local Players = game:GetService("Players")
-local TeleportService = game:GetService("TeleportService")
-local TweenService = game:GetService("TweenService")
-local RunService = game:GetService("RunService")
-local UserInputService = game:GetService("UserInputService")
 local LocalPlayer = Players.LocalPlayer
+local TweenService = game:GetService("TweenService")
+local UserInputService = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
+local TeleportService = game:GetService("TeleportService")
 
+local Venyx = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/Venyx-UI-Library/main/source.lua"))()
 local UI = Venyx.new("JCJM Hub | Steal a Brainrot", 5012544693)
 
--- Full Brainrot List
-local brainrotList = {
-    -- Common
-    "Noobini Pizzanini (Common)", "Lirili Larila (Common)", "Tim Cheese (Common)", "FluriFlura (Common)",
-    "Talpa Di Fero (Common)", "Svinina Bombardino (Common)", "Raccooni Jandelini (Common)",
-    "Pipi Kiwi (Common)", "Pipi Corni (Common)", 
+local pageBrainrots = UI:addPage("Brainrots", 5012544693)
+local pageVisuals  = UI:addPage("Visuals", 5012544693)
+local pageMisc     = UI:addPage("Misc", 5012544693)
+local pageSettings = UI:addPage("Settings", 5012544693)
 
-    -- Rare
-    "Trippi Troppi (Rare)", "Tung Tung Tung Sahur (Rare)", "Gangster Footera (Rare)", "Bandito Bobritto (Rare)",
-    "Boneca Ambalabu (Rare)", "Cacto Hipopotamo (Rare)", "Ta Ta Ta Ta Sahur (Rare)", "Tric Trac Baraboom (Rare)",
-    "Pipi Avocado (Rare)",
-
-    -- Epic
-    "Cappuccino Assassino (Epic)", "Bandito Axolito (Epic)", "Brr Brr Patapim (Epic)", "Avocadini Antilopini (Epic)",
-    "Trulimero Trulicina (Epic)", "Bambini Crostini (Epic)", "Malame Amarele (Epic)", "Bananita Dolphinita (Epic)",
-    "Perochello Lemonchello (Epic)", "Brri Brri Bicus Dicus Bombicus (Epic)", "Avocadini Guffo (Epic)",
-    "Ti Ti Ti Sahur (Epic)", "Mangolini Parrocini (Epic)", "Penguino Cocosino (Epic)", "Salamino Penguino (Epic)",
-
-    -- Legendary
-    "Burbaloni Loliloli (Legendary)", "Chimpanzini Bananini (Legendary)", "Ballerina Cappuccina (Legendary)",
-    "Chef Crabracadabra (Legendary)", "Lionel Cactuseli (Legendary)", "Quivioli Ameleonni (Legendary)",
-    "Glorbo Fruttodrillo (Legendary)", "Caramello Filtrello (Legendary)", "Pipi Potato (Legendary)",
-    "Blueberrini Octopusin (Legendary)", "Strawberelli Flamingelli (Legendary)", "Pandaccini Bananini (Legendary)",
-    "Cocosini Mama (Legendary)", "Pi Pi Watermelon (Legendary)", "Signore Carapace (Legendary)", "Sigma Boy (Legendary)",
-
-    -- Mythic
-    "Frigo Camelo (Mythic)", "Orangutini Ananassini (Mythic)", "Rhino Toasterino (Mythic)", "Bombardiro Crocodilo (Mythic)",
-    "Brutto Gialutto (Mythic)", "Spioniro Golubiro (Mythic Lucky Box)", "Bombombini Gusini (Mythic)",
-    "Zibra Zubra Zibralini (Mythic Lucky Box)", "Tigrilini Watermelini (Mythic Lucky Box)", "Avocadorilla (Mythic)",
-    "Cavallo Virtuso (Mythic)", "Gorillo Subwoofero (Mythic)", "Gorillo Watermelondrillo (Mythic)", "Tob Tobi Tobi (Mythic)",
-    "Lerulerulerule (Mythic)", "Ganganzelli Trulala (Mythic)", "Te Te Te Sahur (Mythic)", "Rhino Helicopterino (Mythic)",
-    "Tracoducotulu Delapeladustuz (Mythic)", "Los Noobinis (Mythic)", "Carloo (Mythic)",
-    "Carrotini Brainini (Mythic Lucky Box)",
-
-    -- Brainrot God
-    "Cocofanto Elefanto (Brainrot God)", "Antonio (Brainrot God)", "Coco Elefanto (Brainrot God)",
-    "Girafa Celestre (Brainrot God)", "Gattatino Nyanino (Brainrot God)", "Chihuanini Taconini (Brainrot God)",
-    "Matteo (Brainrot God)", "Tralalero Tralala (Brainrot God)", "Los Crocodillitos (Brainrot God)",
-    "Trigoligre Frutonni (Brainrot God Lucky Box)", "Espresso Signora (Brainrot God)", "Odin Din Din Dun (Brainrot God)",
-    "Statutino Libertino (Brainrot God)", "Tipi Topi Taco (Brainrot God)", "Unclito Samito (Brainrot God)",
-    "Alessio (Brainrot God)", "Orcalero Orcala (Brainrot God)", "Tralalita Tralala (Brainrot God)",
-    "Tukanno Bananno (Brainrot God)", "Trenostruzzo Turbo 3000 (Brainrot God)", "Urubini Flamenguini (Brainrot God)",
-    "Gattito Tacoto (Brainrot God)", "Trippi Troppi Troppa Trippa (Brainrot God)", "Las Cappuchinas (Brainrot God)",
-    "Ballerino Lololo (Brainrot God)", "Bulbito Bandito Traktorito (Brainrot God Lucky Box)", "Los Tungtungtungcitos (Brainrot God)",
-    "Pakrahmatmamat (Brainrot God)", "Los Bombinitos (Brainrot God)", "Brr es Teh Patipum (Brainrot God)",
-    "Piccione Macchina (Brainrot God)", "Bombardini Tortini (Brainrot God)", "Tractoro Dinosauro (Brainrot God)",
-    "Los Orcalitos (Brainrot God)", "Orcalita Orcala (Brainrot God)", "Cacasito Satalito (Brainrot God)",
-    "Tartaruga Cisterna (Brainrot God)", "Los Tipi Tacos (Brainrot God)", "Piccionetta Macchina (Brainrot God)",
-    "Mastodontico Telepiedone (Brainrot God Lucky Box)", "Anpali Babel (Brainrot God)", "Belula Beluga (Brainrot God)",
-
-    -- Secret
-    "La Vacca Staturno Saturnita (Secret)", "Bisonte Giuppitere (Secret)", "Los Matteos (Secret)", "Karkerkar Kurkur (Secret)",
-    "Trenostruzzo Turbo 4000 (Secret)", "Sammyni Spyderini (Secret)", "Torrtuginni Dragonfrutini (Secret Lucky Box)",
-    "Dul Dul Dul (Secret)", "Blackhole Goat (Secret)", "Agarrini la Palini (Secret)", "Los Spyderinis (Secret)",
-    "Fragola la la la (Secret)", "Los Tralaleritos (Secret)", "Guerriro Digitale (Secret)", "Las Tralaleritas (Secret)",
-    "Job Job Job Sahur (Secret)", "Las Vaquitas Saturnitas (Secret)", "Graipuss Medussi (Secret)", "Noo My Hotspot (Secret)",
-    "Sahur Combinasion (Secret)", "Pot Hotspot (Secret Lucky Box)", "Chicleteira Bicicleteira (Secret)",
-    "Los Nooo My Hotspotsitos (Secret)", "La Grande Combinasion (Secret)", "Los Combinasionas (Secret)",
-    "Nuclearo Dinossauro (Secret)", "Los Hotspotsitos (Secret)", "Tralaledon (Secret)", "Esok Sekolah (Secret)",
-    "Ketupat Kepat (Secret)", "Los Bros (Secret)", "La Supreme Combinasion (Secret)", "Ketchuru and Masturu (Secret)",
-    "Garama and Madundung (Secret)", "Spaghetti Tualetti (Secret)", "Dragon Cannelloni (Secret)",
-
-    -- OG
-    "Strawberry Elephant (OG)"
-}
-
--- Sections
+-- Brainrots stealing section
 local stealingSection = pageBrainrots:addSection("Stealing")
 stealingSection:addButton("Tween to Base Steal (method 1)", function() end)
 stealingSection:addButton("Tween to Base Steal (method 2)", function() end)
 stealingSection:addButton("Float to Base", function() end)
-stealingSection:addButton("Move Up 5 Studs", function() verticalOffset = verticalOffset + 5 updateFloat() end)
-stealingSection:addButton("Move Down 5 Studs", function() verticalOffset = verticalOffset - 5 updateFloat() end)
+stealingSection:addButton("Move Up 5 Studs", function()
+    verticalOffset = verticalOffset + 5
+end)
+stealingSection:addButton("Move Down 5 Studs", function()
+    verticalOffset = verticalOffset - 5
+end)
 stealingSection:addToggle("Steal Boost", false, function(v) end)
 
+-- Brainrots buying section
 local buyingSection = pageBrainrots:addSection("Buying")
-buyingSection:addDropdown("Select Brainrot to Buy", brainrotList, nil, function() end)
+local brainrotsList = {
+    "Noobini Pizzanini (Common)", "Lirili Larila (Common)", "Tim Cheese (Common)", "FluriFlura (Common)", "Talpa Di Fero (Common)",
+    "Svinina Bombardino (Common)", "Raccooni Jandelini (Common)", "Pipi Kiwi (Common)", "Pipi Corni (Common)",
+    "Trippi Troppi (Rare)", "Gangster Footera (Rare)", "Bandito Bobritto (Rare)", "Boneca Ambalabu (Rare)", "Cacto Hipopotamo (Rare)",
+    "Ta Ta Ta Ta Sahur (Rare)", "Tric Trac Baraboom (Rare)", "Pipi Avocado (Rare)", "Cappuccino Assassino (Epic)",
+    "Bandito Axolito (Epic)", "Brr Brr Patapim (Epic)", "Avocadini Antilopini (Epic)", "Trulimero Trulicina (Epic)", "Bambini Crostini (Epic)",
+    "Malame Amarele (Epic)", "Bananita Dolphinita (Epic)", "Perochello Lemonchello (Epic)", "Brri Brri Bicus Dicus Bombicus (Epic)",
+    "Avocadini Guffo (Epic)", "Ti Ti Ti Sahur (Epic)", "Mangolini Parrocini (Epic)", "Penguino Cocosino (Epic)", "Salamino Penguino (Epic)",
+    "Burbaloni Lolololi (Legendary)", "Chimpanzini Bananini (Legendary)", "Ballerina Cappuccina (Legendary)", "Chef Crabracadabra (Legendary)",
+    "Lionel Cactuseli (Legendary)", "Quivioli Ameleonni (Legendary)", "Glorbo Fruttodrillo (Legendary)", "Caramello Filtrello (Legendary)",
+    "Pipi Potato (Legendary)", "Blueberrini Octopusin (Legendary)", "Strawberelli Flamingelli (Legendary)", "Pandaccini Bananini (Legendary)",
+    "Cocosini Mama (Legendary)", "Pi Pi Watermelon (Legendary)", "Signore Carapace (Legendary)", "Sigma Boy (Legendary)",
+    "Frigo Camelo (Mythic)", "Orangutini Ananassini (Mythic)", "Rhino Toasterino (Mythic)", "Bombardiro Crocodilo (Mythic)",
+    "Brutto Gialutto (Mythic)", "Spioniro Golubiro (Mythic Lucky Block)", "Bombombini Gusini (Mythic)", "Zibra Zubra Zibralini (Mythic Lucky Block)",
+    "Tigrilini Watermelini (Mythic Lucky Block)", "Avocadorilla (Mythic)", "Cavallo Virtuso (Mythic)", "Gorillo Subwoofero (Mythic)",
+    "Gorillo Watermelondrillo (Mythic)", "Tob Tobi Tobi (Mythic)", "Lerulerulerule (Mythic)", "Ganganzelli Trulala (Mythic)", "Te Te Te Sahur (Mythic)",
+    "Rhino Helicopterino (Mythic)", "Tracoducotulu Delapeladustuz (Mythic)", "Los Noobinis (Mythic)", "Carloo (Mythic)", "Carrotini Brainini (Mythic Lucky Block)",
+    "Cocofanto Elefanto (Brainrot God)", "Antonio (Brainrot God)", "Coco Elefanto (Brainrot God)", "Girafa Celestre (Brainrot God)", "Gattatino Nyanino (Brainrot God)",
+    "Chihuanini Taconini (Brainrot God)", "Matteo (Brainrot God)", "Tralalero Tralala (Brainrot God)", "Los Crocodillitos (Brainrot God)", "Trigoligre Frutonni (Brainrot God Lucky Block)",
+    "Espresso Signora (Brainrot God)", "Odin Din Din Dun (Brainrot God)", "Statutino Libertino (Brainrot God)", "Tipi Topi Taco (Brainrot God)", "Unclito Samito (Brainrot God)",
+    "Alessio (Brainrot God)", "Orcalero Orcala (Brainrot God)", "Tralalita Tralala (Brainrot God)", "Tukanno Bananno (Brainrot God)", "Trenostruzzo Turbo 3000 (Brainrot God)",
+    "Urubini Flamenguini (Brainrot God)", "Gattito Tacoto (Brainrot God)", "Trippi Troppi Troppa Trippa (Brainrot God)", "Las Cappuchinas (Brainrot God)", "Ballerino Lololo (Brainrot God)",
+    "Bulbito Bandito Traktorito (Brainrot God Lucky Block)", "Los Tungtungtungcitos (Brainrot God)", "Pakrahmatmamat (Brainrot God)", "Los Bombinitos (Brainrot God)",
+    "Brr es Teh Patipum (Brainrot God)", "Piccione Macchina (Brainrot God)", "Bombardini Tortini (Brainrot God)", "Tractoro Dinosauro (Brainrot God)", "Los Orcalitos (Brainrot God)",
+    "Orcalita Orcala (Brainrot God)", "Cacasito Satalito (Brainrot God)", "Tartaruga Cisterna (Brainrot God)", "Los Tipi Tacos (Brainrot God)", "Piccionetta Macchina (Brainrot God)",
+    "Mastodontico Telepiedone (Brainrot God Lucky Block)", "Anpali Babel (Brainrot God)", "Belula Beluga (Brainrot God)", "La Vacca Saturno Saturnita (Secret)",
+    "Bisonte Giuppitere (Secret)", "Los Matteos (Secret)", "La Karkerkar Combinasion (Secret)", "Trenostruzzo Turbo 4000 (Secret)", "Sammyni Spyderini (Secret)",
+    "Torrtuginni Dragonfrutini (Secret Lucky Block)", "Dul Dul Dul (Secret)", "Blackhole Goat (Secret)", "Agarrini la Palini (Secret)", "Los Spyderinis (Secret)",
+    "Fragola la la la (Secret)", "Chimpanzini Spiderini (Secret)", "Tortuginni Dragonfruitini (Secret Lucky Block)", "Los Tralaleritos (Secret)", "Guerriro Digitale (Secret)",
+    "Las Tralaleritas (Secret)", "Job Job Job Sahur (Secret)", "To To To Sahur (Secret)", "La Sahur Combinasion (Secret)", "Las Vaquitas Saturnitas (Secret)",
+    "Graipuss Medussi (Secret)", "Noo My Hotspot (Secret)", "Sahur Combinasion (Secret)", "Pot Hotspot (Secret Lucky Block)", "Chicleteira Bicicleteira (Secret)",
+    "Los Nooo My Hotspotsitos (Secret)", "La Grande Combinasion (Secret)", "Los Combinasionas (Secret)", "Nuclearo Dinossauro (Secret)", "Los Hotspotsitos (Secret)",
+    "Tralaledon (Secret)", "Esok Sekolah (Secret)", "Ketupat Kepat (Secret)", "Los Bros (Secret)", "La Supreme Combinasion (Secret)", "Ketchuru and Masturu (Secret)",
+    "Garama and Madundung (Secret)", "Spaghetti Tualetti (Secret)", "Dragon Cannelloni (Secret)", "Strawberry Elephant (OG)"
+}
 
-local finderSection = pageBrainrots:addSection("Finder")
-finderSection:addDropdown("Select Brainrot to Find", brainrotList, nil, function() end)
+finderSection = pageBrainrots:addSection("Finder")
+finderSection:addDropdown("Select Brainrot to Find", brainrotsList, nil, function() end)
+buyingSection:addDropdown("Select Brainrot to Buy", brainrotsList, nil, function() end)
 
--- Visuals
+-- Floating system
+local verticalOffset = 0
+local floatBP
+RunService.RenderStepped:Connect(function()
+    local char = LocalPlayer.Character
+    if char and char:FindFirstChild("HumanoidRootPart") then
+        local root = char.HumanoidRootPart
+        if not floatBP then
+            floatBP = Instance.new("BodyPosition")
+            floatBP.MaxForce = Vector3.new(0, math.huge, 0)
+            floatBP.P = 1250
+            floatBP.D = 50
+            floatBP.Parent = root
+        end
+        floatBP.Position = root.Position + Vector3.new(0, verticalOffset, 0)
+    end
+end)
+
+-- Visuals section
 local visualsSection = pageVisuals:addSection("Visuals")
 visualsSection:addSlider("Placeholder", 1, 10, 5, function(v) end)
 
 -- Misc
 local miscSection = pageMisc:addSection("Miscellaneous")
 miscSection:addButton("Anti Hit (reset to remove)", function() end)
-miscSection:addButton("Rejoin", function() TeleportService:Teleport(game.PlaceId, LocalPlayer) end)
+miscSection:addButton("Rejoin", function()
+    TeleportService:Teleport(game.PlaceId, LocalPlayer)
+end)
 miscSection:addButton("Server Hop", function()
     local PlaceID = game.PlaceId
     local Success, Servers = pcall(function()
@@ -105,10 +104,13 @@ miscSection:addButton("Server Hop", function()
     if Success and Servers and Servers.data then
         local ServerList = {}
         for _, v in pairs(Servers.data) do
-            if type(v) == "table" and v.id ~= game.JobId then table.insert(ServerList, v.id) end
+            if type(v) == "table" and v.id ~= game.JobId then
+                table.insert(ServerList, v.id)
+            end
         end
         if #ServerList > 0 then
-            TeleportService:TeleportToPlaceInstance(PlaceID, ServerList[math.random(1,#ServerList)], LocalPlayer)
+            local RandomServer = ServerList[math.random(1,#ServerList)]
+            TeleportService:TeleportToPlaceInstance(PlaceID, RandomServer, LocalPlayer)
         end
     end
 end)
@@ -145,27 +147,6 @@ miscSection:addToggle("Noclip", false, function(v)
     end
 end)
 
--- Floating logic
-local verticalOffset = 0
-local floatBP
-function updateFloat()
-    local char = LocalPlayer.Character
-    if char and char:FindFirstChild("HumanoidRootPart") then
-        local root = char.HumanoidRootPart
-        if not floatBP then
-            floatBP = Instance.new("BodyPosition")
-            floatBP.MaxForce = Vector3.new(0, math.huge, 0)
-            floatBP.P = 1250
-            floatBP.D = 50
-            floatBP.Parent = root
-        end
-        floatBP.Position = root.Position + Vector3.new(0, verticalOffset, 0)
-    end
-end
-RunService.RenderStepped:Connect(function()
-    if floatBP then updateFloat() end
-end)
-
 -- Settings
 local settingsSection = pageSettings:addSection("Settings")
 settingsSection:addSlider("UI Scale", 50, 200, 100, function(v)
@@ -191,15 +172,19 @@ uiCorner.Parent = button
 
 local dragging = false
 local dragInput, dragStart, startPos
+
 local function toggleGUI()
     local gui = UI.container
     if gui.Visible then
-        TweenService:Create(gui, TweenInfo.new(0.3), {BackgroundTransparency = 1}):Play()
-        gui.Visible = false
+        local tweenOut = TweenService:Create(gui, TweenInfo.new(0.3), {BackgroundTransparency = 1})
+        tweenOut:Play()
+        tweenOut.Completed:Connect(function() gui.Visible = false end)
         button.Text = "Open"
     else
         gui.Visible = true
-        TweenService:Create(gui, TweenInfo.new(0.3), {BackgroundTransparency = 0}):Play()
+        gui.BackgroundTransparency = 1
+        local tweenIn = TweenService:Create(gui, TweenInfo.new(0.3), {BackgroundTransparency = 0})
+        tweenIn:Play()
         button.Text = "Close"
     end
 end
