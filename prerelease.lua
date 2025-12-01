d613599024ae6114614d7c7020fdcf207cce4150
supa <singh41958@gmail.com>
2025-12-01 16:50:56 -0500

    Update prerelease.lua

--- prerelease.lua
+++ prerelease.lua
@@ -1,9 +1,9 @@
 local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Luna-Interface-Suite/refs/heads/master/source.lua", true))()
 local Window = Luna:CreateWindow({
-    Name = "Supa Hub",
+    Name = "JCJM Hub",
     Subtitle = "Free - Keyless",
     LoadingEnabled = true,
-    LoadingTitle = "Supa Hub",
+    LoadingTitle = "JCJM Hub",
     LoadingSubtitle = "Free - Keyless",
     ConfigSettings = { RootFolder = nil, ConfigFolder = "SupaHub" },
     KeySystem = false
